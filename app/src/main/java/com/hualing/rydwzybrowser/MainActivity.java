c4392f0e27be940e4adc0d831bab87763378e36c
handongqignxue <1417140290@qq.com>
2021-07-05 17:48:18 +0800

    完善

--- app/src/main/java/com/hualing/rydwzybrowser/MainActivity.java
+++ app/src/main/java/com/hualing/rydwzybrowser/MainActivity.java
@@ -2,10 +2,13 @@ package com.hualing.rydwzybrowser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 //https://blog.csdn.net/weixin_40438421/article/details/85700109
+//net：：ERR_CLEARTEXT_NOT_PERMITTED解决方案：https://blog.csdn.net/weixin_44618862/article/details/99611917
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,17 +18,32 @@ public class MainActivity extends AppCompatActivity {
 
         //获得控件
         WebView webView = (WebView) findViewById(R.id.browser_wv);
-        //访问网页
-        webView.loadUrl("http://www.qrcodesy.com:8080/PositionPhZY/phone/goPage?page=login");
-        //系统默认会通过手机浏览器打开网页，为了能够直接通过WebView显示网页，则必须设置
-        webView.setWebViewClient(new WebViewClient(){
-            @Override
+
+        // 设置WebView属性，能够执行Javascript脚本,但是好像不起作用
+        webView.getSettings().setJavaScriptEnabled(true);
+        MyWebChromeClient myWebChromeClient = new MyWebChromeClient();
+        webView.setWebChromeClient(myWebChromeClient);
+
+        try {
+            // 设置打开的页面地址
+            webView.loadUrl("http://192.168.2.166:8080/PositionPhZY/phone/goPage?page=login");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        // 覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
+        webView.setWebViewClient(new WebViewClient() {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                //使用WebView加载显示url
+                // 返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
                 view.loadUrl(url);
-                //返回true
+                // 把初始页面Url加入历史记录
+
                 return true;
             }
         });
+
+        //访问网页
+        //webView.loadUrl("http://192.168.2.166:8080/PositionPhZY/phone/goPage?page=login");
+        //webView.loadUrl("http://www.qrcodesy.com:8080/PositionPhZY/phone/goPage?page=login");
+
     }
 }
